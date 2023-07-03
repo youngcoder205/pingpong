da8da3cb42b8f2ade3688b1c35e2d972969f3688
youngcoder205 <69458400+youngcoder205@users.noreply.github.com>
2023-07-03 18:44:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,13 +23,21 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas")
+  video = createCapture(VIDEO)
+  video.size(700,600)
+  video.hide()
+  poseNet = ml5.poseNet(video,modelloaded )
 }
 
+function modelloaded()  {
+  console.log("model is loaded")
+}
 
 function draw(){
 
  background(0); 
-
+ image(video,0,0,700,600)
  fill("black");
  stroke("black");
  rect(680,0,20,700);
